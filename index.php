5987f540eec31958bce45fd95af2b9f2fd589185
Lian Li <draggili@gmail.com>
2014-05-19 14:36:55 +0200

    some interfacing

--- index.php
+++ index.php
@@ -1,9 +1,15 @@
 <html ng-app>
     <head>
+        <script src="//code.jquery.com/jquery-1.11.1.min.js" type="text/javascript"></script>
         <script src="src/js/angular.min.js" type="text/javascript"></script>
+        <script src="src/js/app.js" type="text/javascript"></script>
+        <link rel="stylesheet" href="src/css/styles.css" type="text/css">
+        <link rel="stylesheet" href="src/css/960.css" type="text/css">
     </head>
     <body>
-        <div><a href="src/game/addGame.php" target="_blank">Add Game</a></div>
-        <div></div>
+        <div class="container_24">
+            <div class="navi grid_3">Add Game</div>
+            <div></div>
+        </div>
     </body>
 </html>
