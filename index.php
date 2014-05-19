1b6e518caed2d031b3fd0c0f98e6c2cf6d3ba078
Lian Li <draggili@gmail.com>
2014-05-19 13:37:35 +0200

    setup stuff

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <html ng-app>
     <head>
-        <script src="js/angular.min.js" type="text/javascript" />
+        <script src="src/js/angular.min.js" type="text/javascript"></script>
     </head>
     <body>
         <div><a href="src/game/addGame.php" target="_blank">Add Game</a></div>
