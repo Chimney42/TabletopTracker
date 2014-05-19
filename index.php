01f070f044dadbe31b29f316eed7eb625fa07002
Lian Li <draggili@gmail.com>
2014-05-19 13:31:42 +0200

    adds link for setup testing

---
+++ index.php
@@ -0,0 +1,9 @@
+<html ng-app>
+    <head>
+        <script src="js/angular.min.js" type="text/javascript" />
+    </head>
+    <body>
+        <div><a href="src/game/addGame.php" target="_blank">Add Game</a></div>
+        <div></div>
+    </body>
+</html>
