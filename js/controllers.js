93a8b63ca7dcbcd4f06714cbcb967501d6ad1d62
Lian Li <draggili@gmail.com>
2014-05-23 14:54:43 +0200

    wip

--- js/controllers.js
+++ js/controllers.js
@@ -1,9 +1,12 @@
-tabletopTracker.controller('NaviController', ['$scope',
+tabletopTracker.controller('NaviController', ['$scope', 
     function($scope) {
         $scope.partial = 'views/index.html';
         
         $scope.items = [
             {
+                'name' : 'List Games',
+                'link' : 'views/game/gameList.html'
+            }, {
                 'name' : 'Add Game',
                 'link' : 'views/game/addGame.html'
             }
@@ -11,6 +14,14 @@ tabletopTracker.controller('NaviController', ['$scope',
         
         $scope.goTo = function(link) {
             $scope.partial = link;
+        };
+    }
+]);
+
+tabletopTracker.controller('GameController', ['$scope', 'dbService',
+    function($scope) {
+        $scope.games = function() {
+            return dbService.getGameList();
         };
     }
 ]);
--- js/services.js
+++ js/services.js
@@ -1,1 +1,22 @@
 var tabletopTracker = angular.module('tabletopTracker', ['ui.bootstrap']);
+
+tabletopTracker.factory('dbService', ['$rootScope', '$http',
+    function($rootScope, $http) {
+        var dbService = function() {
+            this.host = 'http://chimney42.iriscouch.com/tabletoptracker';
+            
+            this.getGameList = function() {
+                var self = this;
+                url = self.host + '_design/games/_view/list';
+
+                $http({method: 'GET', url: url}).
+                success(function(data, status, headers, config) {
+                    console.log(data);
+                }).
+                error(function(data, status, headers, config) {
+                  console.log('error getting game list');
+                });
+            };
+        };
+    }
+]);
